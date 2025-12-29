49969cff96b9b70b48e00c9b94b819ab64885fca
inait DevOps <113030883+arturos-inait@users.noreply.github.com>
2025-12-30 00:41:30 +0100

    Update TrackReconstructor.hpp

--- include/TrackReconstructor.hpp
+++ include/TrackReconstructor.hpp
@@ -1,50 +1,36 @@
-// CSC Latin America 2026 - Track Reconstructor
-// This file contains intentional bugs for Exercise E1!
 #pragma once
 
-#include "Particle.hpp"
 #include <vector>
-#include <memory>
 
 namespace csc2026 {
 
 struct Hit {
-    double x, y, z;
-    double energyDeposit;
+    double x{};
+    double y{};
+    double z{};
+    double energy{};
 };
 
 struct Track {
-    std::vector<Hit> hits;
-    double pt;
-    double eta;
-    double phi;
-    
-    double chi2() const;
+    double pt{};
+    std::vector<Hit> hits{};
 };
 
 class TrackReconstructor {
 public:
-    TrackReconstructor(int maxHits = 100);
-    ~TrackReconstructor();
-    
-    // Add a hit to the current event
+    explicit TrackReconstructor(double minPt);
+
     void addHit(const Hit& hit);
-    
-    // Reconstruct tracks from accumulated hits
-    std::vector<Track> reconstruct();
-    
-    // Clear hits for next event
-    void clear();
-    
-    // Get number of hits
-    size_t numHits() const { return m_hits.size(); }
-    
+
+    std::vector<Track> reconstruct() const;
+
+    const Track* getBestTrack() const;
+
 private:
-    std::vector<Hit> m_hits;
-    int m_maxHits;
-    
-    // Internal helper (may contain bugs!)
-    void processHits();
+    double m_minPt{0.0};
+    std::vector<Hit> m_hits{};
+    mutable Track m_bestTrack{};
 };
 
 } // namespace csc2026
+
